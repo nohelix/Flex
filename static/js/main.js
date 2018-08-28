3a3b74a61e82cd039bc2cc6a661cb4dcb9797337
nohelix <github@nohelix.com>
2018-08-27 21:09:17 -0500

    Added Colorbox to theme

---
+++ static/js/main.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function($){$('p').each(function(index){$(this).find('a[href$="jpg"],a[href$="jpeg"],a[href$="png"],a[href$="gif"],a[href$="tif"]').colorbox({maxWidth:'100%',maxHeight:'100%',rel:'group1'.concat(index)});});});
